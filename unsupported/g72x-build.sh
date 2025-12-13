55a7b8246c8ad3f32e50a033529e5a52c11a5592
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2025-12-13 22:19:44 +0200

    Add Asterisk 22 binaries

--- unsupported/g72x-build.sh
+++ unsupported/g72x-build.sh
@@ -178,9 +178,9 @@ popd
 srcdir=.
 mkdir -p bin/
 
-a=ast210
+a=ast220
 ast=/opt/asterisk
-def2="-DG72X_ASTERISK=210"
+def2="-DG72X_ASTERISK=220"
 codec=g729 dir=$srcdir all &
 codec=g723 dir=$srcdir all &
 
